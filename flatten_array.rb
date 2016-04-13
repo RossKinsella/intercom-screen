c10bd347abd6caf17b59631651488fe9eea155d0
Ross Kinsella <rosskinsella0@gmail.com>
2016-04-14 00:31:03 +0100

    Flatten array tests

--- flatten_array.rb
+++ flatten_array.rb
@@ -1,9 +1,3 @@
-# Write a function that will flatten an array of arbitrarily nested arrays of integers into a flat array of integers.
-# e.g. [[1,2,[3]],4] → [1,2,3,4]. If the language you're using has a function to flatten arrays, you should pretend it doesn't exist.
-
-# Excellent answers have a complete test suite covering empty lists, invalid input, null-hostile checks.
-# If this code was in a code review on your team you’d spend time picking a well deserved “ship it” emoji or GIF.
-
 def is_array? array
   array.class == Array
 end
@@ -18,12 +12,10 @@ def flatten array
   array.each do |item|
     if item.class == Array
       flattened_array = flattened_array + (flatten item)
-    else
+    elsif item.class != NilClass
       flattened_array.push item
     end
   end
 
   flattened_array
 end
-
-puts flatten [1,[2,[[[[42,[[[[9],8]]]]]]]],3]
---
+++ flatten_array_tests.rb
@@ -0,0 +1,42 @@
+require 'test/unit'
+require './flatten_array.rb'
+
+class FlattenArrayTest < Test::Unit::TestCase
+  def test_nested_arrays
+    array = [2,[3,5,7],[[11,[[13,[[[[[17],19]]]]]]]]]
+    assert_equal(flatten(array), [2,3,5,7,11,13,17,19])
+  end
+
+  def test_already_flat_array
+    array = [2,3,5]
+    assert_equal(flatten(array), [2,3,5])
+  end
+
+  def test_empty_array
+    array = []
+    assert_equal(flatten(array), [])
+  end
+
+  def test_nested_empty_arrays
+    array = [[[[]]]]
+    assert_equal(flatten(array), [])
+  end
+
+  def test_null_item
+    array = [2,3,[5,nil,7],[[[nil,[[19]]]]]]
+    assert_equal(flatten(array), [2,3,5,7,19])
+  end
+
+  def test_invalid_input
+    non_array = false
+    assert_raise(ArgumentError) {
+      flatten(non_array)
+    }
+  end
+
+  def test_mixed_item_types
+    array = [2, "a", [:a, [], test_empty_array,[true, {'item' => 3}]]]
+    assert_equal(flatten(array), [2,"a", :a, test_empty_array, true,{'item' => 3}])
+  end
+
+end
